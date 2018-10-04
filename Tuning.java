eae2b7595da359fde1acf2765190de026307864f
Narendiran CG <cgnarendiran@gmail.com>
2018-10-04 13:54:21 +0530

    Tuning framework building1

--- Tuning.java
+++ Tuning.java
@@ -2,6 +2,35 @@ import java.util.Arrays;
 import java.util.Random;
 import java.io.*;
 
+
+
+public class Parameter_vector{  
+    String selector;
+    String recombinator;
+    String mutator;
+    int population_size;
+    double mutation_size;
+    // performance score of the parameter vector
+    double score = 0.0; 
+    // Constructor:
+    Parameter_vector(String selector, String recombinator, String mutator, int population_size, double mutation_size){  
+        this.selector=selector;  
+        this.recombinator=recombinator;  
+        this.mutator=mutator;
+        this.population_size=population_size;
+        this.mutation_size=mutation_size;
+        this.score = score;
+    }
+    public void setVariables() {
+    	Var.setSELECTOR(this.selector);
+		Var.setRECOMBINATOR(this.recombinator);
+		Var.setMUTATOR(this.mutator);
+		Var.setPOPULATION_SIZE(this.population_size);
+		Var.setMUTATION_RATE(this.mutation_size);
+
+    } 
+} 
+
 public class Tuning{
 
 	public static String[] selector = {"parentSelection_elitism","parentSelection_rankBased_LR", "parentSelection_nr1only"};
@@ -15,28 +44,30 @@ public class Tuning{
 	public String executeCommand(String command) {
 		StringBuffer output = new StringBuffer();
 	    Process p;
-	    String line = ""; 
 	    try {
+	    	System.out.println(command);
 	        p = Runtime.getRuntime().exec(command);
 	        p.waitFor();
 	        BufferedReader reader = 
 	                        new BufferedReader(new InputStreamReader(p.getInputStream()));
+	        String line = ""; 
+	        System.out.println(p.getInputStream());
+	        while ((line = reader.readLine())!= null) {
 
-	                  
-	        // while ((line = reader.readLine())!= null) {
-	        //     output.append(line + "\n");
-	        // }
-	        while (reader.ready()){
-	        	line = reader.readLine();
-	        	// output.append(line);
+	            output.append(line + "\n");
 	        }
+	        // while (reader.ready()){
+	        // 	line = reader.readLine();
+	        // 	// output.append(line);
+	        // }
 
 	    } catch (Exception e) {
 	        e.printStackTrace();
 	    }
+	    System.out.println(output.toString());
 
-	    // return output.toString();
-	    return line;
+	    return output.toString();
+	    // return line;
 }
 
 
@@ -44,22 +75,36 @@ public class Tuning{
 	public static void main(String args[]) {
 
 		Tuning com = new Tuning();
+		Parameter_vector[] pv = new Parameter_vector[20];
 
 		// Creating random parameter vectors
-		Var.SELECTOR = Tuning.selector[new Random().nextInt(Tuning.selector.length)];
-		Var.RECOMBINATOR = Tuning.recombinator[new Random().nextInt(Tuning.recombinator.length)];
-		Var.MUTATOR = Tuning.mutator[new Random().nextInt(Tuning.mutator.length)];
-		Var.POPULATION_SIZE = Tuning.min_population_size + (int)(Math.random() * ((Tuning.max_population_size - Tuning.min_population_size) + 1));
-		Var.MUTATION_RATE = (Math.random() * (Tuning.max_mutation_rate - Tuning.min_mutation_rate)) + Tuning.min_mutation_rate;
+		for (i=0; i=i+1; i<20){
+			pv[i] = {Tuning.selector[new Random().nextInt(Tuning.selector.length)],Tuning.recombinator[new Random().nextInt(Tuning.recombinator.length)],
+				Tuning.mutator[new Random().nextInt(Tuning.mutator.length)],Tuning.min_population_size + (int)(Math.random() * ((Tuning.max_population_size - Tuning.min_population_size) + 1)),
+				(Math.random() * (Tuning.max_mutation_rate - Tuning.min_mutation_rate)) + Tuning.min_mutation_rate}
+		}
 
-		// Print all the values:
-		System.out.println(Var.SELECTOR+"\t"+Var.RECOMBINATOR+"\t"+Var.MUTATOR+"\t"+Var.POPULATION_SIZE+"\t"+Var.MUTATION_RATE);
+		// Execute cases:
+		for (i=0; i=i+1; i<20){
+			pv[i].setVariables();
+			// Print all the values for reference:
+			System.out.println(Var.SELECTOR+"\t"+Var.RECOMBINATOR+"\t"+Var.MUTATOR+"\t"+Var.POPULATION_SIZE+"\t"+Var.MUTATION_RATE);
+			// Print the result:
+			System.out.println(com.executeCommand("ls && javac -cp contest.jar group62.java Var.java Mutant.java Population.java EvolutionAlgorithm.java Mutator.java Selector.java Recombinator.java MutantStorage.java Cholesky.java  &&  jar cmf MainClass.txt submission.jar group62.class Mutant.class Var.class Population.class EvolutionAlgorithm.class Mutator.class Selector.class Recombinator.class MutantStorage.class Cholesky.class  &&  java -jar testrun.jar -submission=group62 -evaluation=BentCigarFunction -seed=1"));
+			String result = com.executeCommand("ls && javac -cp contest.jar group62.java Var.java Mutant.java Population.java EvolutionAlgorithm.java Mutator.java Selector.java Recombinator.java MutantStorage.java Cholesky.java  &&  jar cmf MainClass.txt submission.jar group62.class Mutant.class Var.class Population.class EvolutionAlgorithm.class Mutator.class Selector.class Recombinator.class MutantStorage.class Cholesky.class  &&  java -jar testrun.jar -submission=group62 -evaluation=BentCigarFunction -seed=1");
+			// Parse the result and obtain performance.
+			double performance = 0.0
+			pv[i].score = performance;
+			
+		}
+		
+		
 
+		
 		// Execute commands:
 	    
-	    System.out.println(com.executeCommand("javac -cp contest.jar group62.java Var.java Mutant.java Population.java EvolutionAlgorithm.java Mutator.java Selector.java Recombinator.java MutantStorage.java Cholesky.java"));
-	    System.out.println(com.executeCommand("jar cmf MainClass.txt submission.jar group62.class Mutant.class Var.class Population.class EvolutionAlgorithm.class Mutator.class Selector.class Recombinator.class MutantStorage.class Cholesky.class"));
-	    System.out.println(com.executeCommand("java -jar testrun.jar -submission=group62 -evaluation=BentCigarFunction -seed=1"));
+	    	    // System.out.println(com.executeCommand(""));
+	    // System.out.println(com.executeCommand(""));
 
 }
 
--- Var.java
+++ Var.java
@@ -7,9 +7,6 @@ public class Var {
     public static final int SEARCH_SPACE_MAX = 5;
 
 
-    public static int POPULATION_SIZE = 10;
-    public static double MUTATION_RATE = 0.01;
-
     public static final double TAU_1 = 1 / Math.sqrt(2 * NUMBER_OF_GENES);
     public static final double TAU_2 = 1 / Math.sqrt(2 * Math.sqrt(NUMBER_OF_GENES));
     public static final double BOUNDRY = 0.1;
@@ -18,10 +15,33 @@ public class Var {
     public static final double TAU_MOMENTUM_MU = 1/4;
     public static final double TAU_MOMENTUM_SIGMA = 1/2;
 
+
+
+    public static int POPULATION_SIZE = 10;
+    public static double MUTATION_RATE = 0.01;
     public static String SELECTOR = "parentSelection_nr1only";
     public static String RECOMBINATOR = "crossoverPopulation_nPoint";
     public static String MUTATOR = "mutatePopulation_CMN";
+
+    public static void setPOPULATION_SIZE (int p) {
+        POPULATION_SIZE = p;
+    }
     
+    public static void setMUTATION_RATE (double r) {
+        MUTATION_RATE = r;
+    }
+
+    public static void setSELECTOR (String s) {
+        SELECTOR = s;
+    }
+
+    public static void setRECOMBINATOR (String s) {
+        RECOMBINATOR = s;
+    }
+
+    public static void setMUTATOR (String s) {
+        MUTATOR = s;
+    }
 
 
 
