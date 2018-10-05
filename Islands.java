2884b2a8e94718871e0543572783bc6c819a68d1
Tessa Cramwinckel <tessa@wcw-stud-145-109-30-18.wireless.uva.nl>
2018-10-05 14:44:50 +0200

    arraylist trying

--- Islands.java
+++ Islands.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+
 public class Islands {
     /*
     initializing
      */
     int n_islands;
-    private Population[] populations;
+    //private Population[] populations;
+    private ArrayList populations = new ArrayList();
     private EvolutionAlgorithm[] evolutionAlgorithms;
 
     /*
@@ -11,11 +14,10 @@ public class Islands {
      */
     public Islands(int n_islands){
         this.n_islands = n_islands;
-        populations = new Population[n_islands];
         evolutionAlgorithms = new EvolutionAlgorithm[n_islands];
         for (int i = 0; i < n_islands; i++){
-            populations[i] = new Population(Var.POPULATION_SIZE);
-            evolutionAlgorithms[i] = new EvolutionAlgorithm(populations[i]);
+            populations.add(new Population(Var.POPULATION_SIZE));
+            evolutionAlgorithms[i] = new EvolutionAlgorithm(populations.get(i));
         }
     }
 
@@ -23,6 +25,14 @@ public class Islands {
         for (int i = 0; i < n_islands; i++){
             evolutionAlgorithms[i].evolve();
         }
+    }
+
+    public void removeIsland(int islandNr){
+        populations.remove();
+    }
+
+    public void addIsland(){
+
     }
 
     public Population[] getPopulations(){
